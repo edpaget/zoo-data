44f2446fc6448ca82d4041d4ea50d4155f06b7ae
Edward Paget <ed.paget@gmail.com>
2013-09-09 15:00:21 -0500

    Add cors headers to responses

--- src/zoo_data/web/routes.clj
+++ src/zoo_data/web/routes.clj
@@ -6,6 +6,7 @@
                                              POST 
                                              PUT  
                                              DELETE 
+                                             defroutes
                                              context]]
             [compojure.route :as route]
             [zoo-data.model.collection :as c]))
@@ -27,23 +28,37 @@
   {:status 204
    :body ""})
 
+(defn wrap-cors
+  [handler]
+  (fn [req] (let [response (handler req)]
+              (update-in response 
+                         [:headers] 
+                         merge 
+                         {"Access-Control-Allow-Origin" "*"
+                          "Access-Control-Allow_headers" "content-type"
+                          "Access-Control-Allow-Methods" "GET OPTIONS PUT POST DELETE"}))))
+
+(defroutes app-routes
+  (cmpj/routes
+    (OPTIONS "/*" [] (resp-ok ""))
+    (context "/user/:user-id" [user-id] 
+             (context "/project/:project-name" [project-name]
+                      (context "/collection" []
+                               (GET "/" [] 
+                                    (resp-ok (c/find-by-user-and-project user-id project-name)))
+                               (GET "/:id" [id] 
+                                    (resp-ok (c/find-by-id id)))
+                               (POST "/" [params]
+                                     (resp-created (c/create user-id project-name params)))
+                               (PUT "/:id" [id params] 
+                                    (resp-ok (c/update-col id params)))
+                               (DELETE "/:id" [id]
+                                       (resp-no-content (c/delete-col id)))
+                               (GET "/:id/data" [id] (resp-ok (c/get-data id project-name))))))))
+
 (defn routes
   []
-  (let [handler (cmpj/routes
-                  (context "/user/:user-id" [user-id] 
-                           (context "/project/:project-name" [project-name]
-                                    (context "/collection" []
-                                             (GET "/" [] 
-                                                  (resp-ok (c/find-by-user-and-project user-id project-name)))
-                                             (GET "/:id" [id] 
-                                                  (resp-ok (c/find-by-id id)))
-                                             (POST "/" [params]
-                                                   (resp-created (c/create user-id project-name params)))
-                                             (PUT "/:id" [id params] 
-                                                  (resp-ok (c/update-col id params)))
-                                             (DELETE "/:id" [id]
-                                                     (resp-no-content (c/delete-col id)))
-                                             (GET "/:id/data" [id] (resp-ok (c/get-data id project-name)))))))]
-    (-> (wrap-json-response handler)
-        wrap-json-params
-        wrap-stacktrace)))
+  (-> (wrap-json-response app-routes)
+      wrap-json-params
+      wrap-cors
+      wrap-stacktrace))
